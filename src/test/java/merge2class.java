fe856b0aef5abc77679106c4bcbebf21471659fc
Abhinav <abhinavsrivastava7580@gmail.com>
2024-02-02 23:57:04 -0800

    code 4 5 from QA

--- src/test/java/merge2class.java
+++ src/test/java/merge2class.java
@@ -3,5 +3,7 @@ public class merge2class {
     public static void main(String[] args) {
         System.out.println("code 1 from QA");
         System.out.println("code 2 from QA");
+        System.out.println("code 3 from QA");
+        System.out.println("code 4 from QA");
     }
 }
