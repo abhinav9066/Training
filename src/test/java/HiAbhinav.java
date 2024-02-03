020a35a620745b3acdfcea0a5a6fea13cae9ad62
Abhinav <abhinavsrivastava7580@gmail.com>
2024-02-02 22:45:11 -0800

    commit HiAbhinav.java

--- src/test/java/HiAbhinav.java
+++
@@ -1,8 +0,0 @@
-public class HiAbhinav {
-
-    public static void main(String[] args) {
-        System.out.println("biscuit");
-        System.out.println("abcd");
-
-    }
-}
--- src/test/java/NewJava.java
+++
@@ -1,7 +0,0 @@
-public class NewJava {
-
-    public static void main(String[] args) {
-        //adding a new file
-        //hhhgggg
-    }
-}
