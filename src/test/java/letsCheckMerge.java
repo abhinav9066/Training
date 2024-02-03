f5384f92220db1ac3a41a0565abfb078a841809a
Abhinav <abhinavsrivastava7580@gmail.com>
2024-02-02 23:58:22 -0800

    code 67 from INT

--- src/test/java/letsCheckMerge.java
+++ src/test/java/letsCheckMerge.java
@@ -2,7 +2,8 @@ public class letsCheckMerge {
     public static void main(String[] args) {
         System.out.println("code 1 from QA");
         System.out.println("code 2 from QA");
-        System.out.println("code 3 from INT");
+        System.out.println("code 6 from INT");
+        System.out.println("code 7 from INT");
 
     }
 }
