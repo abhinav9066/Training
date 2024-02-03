39574e1fe487fda0509e05f3328c0b24283bf303
Abhinav <abhinavsrivastava7580@gmail.com>
2024-02-02 23:10:14 -0800

    commit code3 and code 4 From QA

--- src/test/java/letsCheckMerge.java
+++ src/test/java/letsCheckMerge.java
@@ -1,7 +1,7 @@
 public class letsCheckMerge {
     public static void main(String[] args) {
-        System.out.println("code 1 from INT");
-        System.out.println("code 1 from INT");
+        System.out.println("code 1 from QA");
+        System.out.println("code 2 from QA");
 
     }
 }
